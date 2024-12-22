ee02639621356537da1cfb63ed02d9a882628305
bluelunace <bluelunace@qq.com>
2024-12-23 00:38:49 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '9baee4f7-594e-4296-a026-3fcc153775bb';
 let proxyIP = '';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
